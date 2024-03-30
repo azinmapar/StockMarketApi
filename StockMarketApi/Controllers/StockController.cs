6c7ebe5c0193a8de285815bcaec4077603a529b4
azinmapar <azin.mapar@yahoo.com>
2024-03-30 19:55:34 +0330

    stock controller

---
+++ StockMarketApi/Controllers/StockController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockMarketApi.Data;
+
+namespace StockMarketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StockController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       // [Route("api/[controller]/GetAllStocks")]
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+       // [Route("api/[controller]/GetStockById")]
+        [HttpGet("id")]
+
+        public IActionResult GetById(int id) 
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null) 
+            {
+                return NotFound();
+            } else 
+            {
+                return Ok(stock);
+            }
+
+        }
+
+    }
+}
